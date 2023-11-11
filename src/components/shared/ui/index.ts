02985fd0dba82926f82de0284e94489659b06d29
alireza yegane <alirezaporyegane@gmail.com>
2023-11-11 14:18:57 +0330

    feat: Add Breadcrumbs

---
+++ src/components/shared/ui/index.ts
@@ -0,0 +1,7 @@
+import Badge from './Badge'
+import Breadcrumbs, { IOption } from './Breadcrumbs'
+import Button from './Button'
+import Card from './Card'
+import Table, { IHead } from './Table'
+
+export { Badge, Breadcrumbs, Button, Card, Table, type IHead, type IOption }
