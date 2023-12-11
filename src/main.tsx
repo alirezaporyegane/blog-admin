235fc60e5978c17807c3c4f5b3f14c13c8851ed5
alireza yegane <alirezaporyegane@gmail.com>
2023-12-11 17:22:19 +0330

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -6,20 +6,24 @@ import { ThemeProvider } from '@mui/material'
 import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { ErrorComponents } from './components/shared/Error/index.tsx'
+import { ErrorBoundary } from './components/shared/ErrorBoundary/index.tsx'
 import { HeaderNameProvider } from './context/HeaderNameContext.tsx'
 import './css/index.scss'
 import { router } from './router/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <AccountProvider>
-    <HelmetProvider>
-      <CacheProvider value={cacheRtl}>
-        <ThemeProvider theme={theme}>
-          <HeaderNameProvider>
-            <RouterProvider router={router} />
-          </HeaderNameProvider>
-        </ThemeProvider>
-      </CacheProvider>
-    </HelmetProvider>
-  </AccountProvider>
+  <ErrorBoundary fallback={<ErrorComponents />}>
+    <AccountProvider>
+      <HelmetProvider>
+        <CacheProvider value={cacheRtl}>
+          <ThemeProvider theme={theme}>
+            <HeaderNameProvider>
+              <RouterProvider router={router} />
+            </HeaderNameProvider>
+          </ThemeProvider>
+        </CacheProvider>
+      </HelmetProvider>
+    </AccountProvider>
+  </ErrorBoundary>
 )
