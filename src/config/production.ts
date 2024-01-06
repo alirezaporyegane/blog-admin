5ad570ae98b5fea86f4db33cfbefee3bbb83b0fd
alireza yegane <alirezaporyegane@gmail.com>
2024-01-06 10:13:41 +0330

    fix: Change Api Server Url

--- src/config/production.ts
+++ src/config/production.ts
@@ -1,7 +1,7 @@
 import { IConfig } from ".";
 
 export default {
-  apiServer: '192.168.77.220:6565',
+  apiServer: 'blog-api-orcin-five.vercel.app',
   thumbServer: '192.168.77.220:8007/unsafe',
   ssl: true
 } as IConfig
